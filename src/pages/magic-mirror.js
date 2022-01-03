ea56a36c484e355e0111ba971a0f6c1cc10a66d5
flaprimo <fla.primo.engineer@outlook.com>
2022-01-03 10:31:02 +0100

    add archillect and portrait mode

--- src/pages/magic-mirror.js
+++ src/pages/magic-mirror.js
@@ -4,6 +4,8 @@ import { graphql } from "gatsby";
 import PropTypes from "prop-types";
 import "bulma/css/bulma.css";
 import cheerio from 'cheerio';
+import axios from 'axios';
+import me from "../../static/force-portrait-orientation.css";
 
 // Window Size
 let defaultHeight
@@ -37,30 +39,81 @@ const useWindowSize = () => {
   return dimensions
 }
 
-// function ArchillectFetch() {
-//   https.get('https://archillect.com/archive', (resp) => {
-//     let data = '';
+function ArchillectFetch() {
+  const [imageUrl, setImageUrl] = useState([]);
+  const baseUrl = 'https://archillect.com/'
 
-//     // A chunk of data has been received.
-//     resp.on('data', (chunk) => {
-//       data += chunk;
-//     });
+  const getArchiveMaxPostId = async() => {
+    try {
+      const response = await axios.get(baseUrl + 'archive');
+      const $ = cheerio.load(response.data); 
+      const maxPostId = $('#archive .post:first').attr('href').substring(1);
+      console.log(maxPostId);
+      
+      return maxPostId;
+    } catch (error) {
+      console.error(error);
+    }
+  }
 
-//     // The whole response has been received. Print out the result.
-//     resp.on('end', () => {
-//       const $ = cheerio.load(data);
-//       $('.apple', '#fruits').text();
-//     });
+  const getArchiveRandomImage = async() => {
+    const maxPostId = await getArchiveMaxPostId();
+    if (!isNaN(maxPostId)) {
+      const postId = Math.floor(Math.random() * (maxPostId - 0) + maxPostId);
 
-//   }).on("error", (err) => {
-//     console.log("Error: " + err.message);
-//   });
+      try {
+        const response = await axios.get(baseUrl.concat(postId));
+        const $ = cheerio.load(response.data); 
+        const imageUrl = $('#ii').attr('src');
+        console.log(imageUrl);
+        
+        return imageUrl;
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      return 'https://www.rd.com/wp-content/uploads/2021/01/GettyImages-1175550351.jpg';
+    }
+  }
 
-//   return (
-//     <img src="">
-//     </img>
-//   );
-// }
+  useEffect(() => {
+    async function fetchArchillectAPI() {
+      const imageUrl = await getArchiveRandomImage();
+      setImageUrl(imageUrl);
+    }
+
+    const grabImageTimer = setInterval(() => {
+      fetchArchillectAPI();
+    }, 10000);
+
+    return () => {
+      clearInterval(grabImageTimer); // Return a funtion to clear the timer so that it will stop being called on unmount
+    }
+  }, [imageUrl]);
+
+  return (
+    <div className="archillect-bg-image" style={{
+      backgroundSize: "contain",
+      width: "100vw",
+      height: "100vh",
+      backgroundRepeat: "no-repeat",
+      backgroundPosition: "center center",
+      backgroundSize: "cover",
+      background: "linear-gradient(rgba(0,0,0,.5), rgba(0,0,0,.8)), url(" + imageUrl + ")"
+    }}>
+      <div className="archillect-image" style={{
+        backgroundImage: "url(" + imageUrl + ")",
+        backgroundSize: "contain",
+        width: "100vw",
+        height: "100vh",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center center",
+        backgroundSize: "contain",
+        backdropFilter: "blur(8px)"
+      }} />
+    </div>
+  );
+}
 
 function MatrixYoutube() {
   return (
@@ -76,7 +129,7 @@ function MatrixYoutube() {
           height: "200vh",
           top: "-40%",
           left: "-50%"
-      }} src="https://www.youtube.com/embed/_Lo6Vup6khc?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&mute=1&hd=1&quality=high" height="1080" frameborder="0" allowfullscreen></iframe>
+      }} src="https://www.youtube.com/embed/_Lo6Vup6khc?controls=0&showinfo=0&rel=0&autoplay=1&loop=1&mute=1&hd=1&quality=high&vq=1080" height="1080" frameBorder="0" allowFullScreen></iframe>
     </div>
   );
 }
@@ -95,7 +148,7 @@ function Clock() {
   }, []);
 
   return (
-    <p className="title">
+    <p className="is-size-1 has-text-weight-bold">
       {time.toLocaleString()}
     </p>
   );
@@ -138,17 +191,16 @@ function MagicMirrorPage(props) {
     <div>
       <Helmet>
         <title>{title}</title>
+        <html className="force-portrait-orientation" lang="en-US"/>
       </Helmet>
       <section ref={firstSection} className="hero is-danger is-fullheight">
         <div className="hero-body">
-          <p className="title">
-            It's a kind of magic ✨
-          </p>
+          <Clock />
         </div>
       </section>
-      <section ref={secondSection} className="hero is-warning is-fullheight">
-        <div className="hero-body">
-          <Clock />
+      <section ref={secondSection} className="hero is-black is-fullheight">
+        <div className="hero-body" style={{padding: 0}}>
+          <ArchillectFetch />
         </div>
       </section>
       <section ref={thirdSection} className="hero is-white is-fullheight">
