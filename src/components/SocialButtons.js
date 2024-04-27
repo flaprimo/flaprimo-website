8191d76426931a085cd90f18e059c1eef1d6e815
flaprimo <fla.primo.engineer@outlook.com>
2024-04-27 14:09:34 +0200

    update social buttons

--- src/components/SocialButtons.js
+++ src/components/SocialButtons.js
@@ -1,26 +1,30 @@
 import React from "react";
 import { SocialIcon } from "react-social-icons";
 import PropTypes from "prop-types";
-import { StaticQuery, graphql } from "gatsby";
+import { useStaticQuery, graphql } from "gatsby";
 
 // http://jaketrent.github.io/react-social-icons/
 
 const SocialButtons = ({ data }) => (
   <div>
-    <ul style={{
-      "listStyleType": "none",
-      "margin": 0,
-      "padding": 0
-    }}>
+    <ul
+      style={{
+        listStyleType: "none",
+        margin: 0,
+        padding: 0,
+      }}
+    >
       {data.site.siteMetadata.social.map((social, i) => (
         <li key={i} style={{ display: "inline-block", marginRight: "10px" }}>
-          <SocialIcon url={social.baseurl + social.user}
-                      color="#363636"
-                      rel="me"
-                      style={{
-                        height: 40,
-                        width: 40
-                      }}/>
+          <SocialIcon
+            url={social.baseurl + social.user}
+            color="#363636"
+            rel="me"
+            style={{
+              height: 40,
+              width: 40,
+            }}
+          />
         </li>
       ))}
     </ul>
@@ -28,31 +32,30 @@ const SocialButtons = ({ data }) => (
 );
 
 /* Query */
-export default props => (
-  <StaticQuery
-    query={graphql`
-      query SocialButtonsQuery {
-        site {
-          siteMetadata {
-            social {
-              title,
-              user,
-              baseurl
-            }
+export default function SocialButtonsInstance(props) {
+  const data = useStaticQuery(graphql`
+    query SocialButtonsQuery {
+      site {
+        siteMetadata {
+          social {
+            title
+            user
+            baseurl
           }
         }
       }
-    `}
-    render={data => <SocialButtons data={data} {...props} />}
-  />
-)
+    }
+  `);
+
+  return <SocialButtons data={data} {...props} />;
+}
 
 SocialButtons.propTypes = {
   data: PropTypes.shape({
     site: PropTypes.shape({
       siteMetadata: PropTypes.shape({
-        social: PropTypes.array.isRequired
-      }).isRequired
-    }).isRequired
-  }).isRequired
+        social: PropTypes.array.isRequired,
+      }).isRequired,
+    }).isRequired,
+  }).isRequired,
 };
